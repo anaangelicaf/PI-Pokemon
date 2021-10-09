261a5a62d7cabfb8669d8cc55e043a4d06c24151
anaangelicaf <86435558+anaangelicaf@users.noreply.github.com>
2021-10-08 22:15:38 -0300

    front 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import { useEffect } from "react";
+import { Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Landing } from "./components/Landing";
+import { Home } from './components/Home.jsx';
+import { Navbar } from "./components/Navbar";
+
+import { Create  } from './components/NewPoke.jsx';
+import { getPokemons, getTypes } from "./redux/actions/actions";
+
+import { Pokemon } from "./components/Pokemon.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
-  );
-}
-
-export default App;
+ 
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(getTypes());
+      dispatch(getPokemons());
+    });
+  
+    return (
+      <>
+        <Navbar />
+        <Route exact path="/pokemons/:id" >
+          <Pokemon />
+        </Route>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/home">
+          <Home />
+        </Route>
+        <Route exact path="/create">
+          <Create />
+        </Route>
+        
+      </>
+    );
+  }
+  
+  export default App;
